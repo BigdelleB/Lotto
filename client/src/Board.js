e7d5eb878c91926069c44219cdc483ff15232318
Brian Bigdelle <bb586@cornell.edu>
2020-12-25 17:21:58 -0500

    Forgot about Client.

---
+++ client/src/Board.js
@@ -0,0 +1,26 @@
+import './Board.css';
+
+function rand_button(props){
+	return(
+		<div className ="rand_button">
+			<h1>
+				Hello
+			</h1>
+		</div>
+		);
+}
+
+
+
+function Board(){
+	return (
+		<div className="Board">
+			<div>
+  				<rand_button but/>
+  			</div>
+  		</div>
+		);
+}
+
+
+export default Board;
---
+++ client/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Board from './Board';
+//import * as serviceWorker from './serviceWorker';
+
+// import drizzle functions and contract artifact
+import { Drizzle, generateStore } from "@drizzle/store";
+import Game from "./contracts/Game.json";
+import LTO from "./contracts/LTO.json";
+
+
+// let drizzle know what contracts we want and how to access our test blockchain
+const options = {
+  contracts: [Game, LTO],
+  web3: {
+    fallback: {
+      type: "ws",
+      url: "ws://127.0.0.1:9545",
+    },
+  },
+};
+
+// setup the drizzle store and drizzle
+const drizzle = new Drizzle(options);
+
+//ReactDOM.render(<App drizzle={drizzle} />, document.getElementById('root'));
+ReactDOM.render(<Board drizzle={drizzle} />, document.getElementById('root'));
