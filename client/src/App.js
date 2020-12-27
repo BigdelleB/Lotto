35b9f8e33ade80f3ad5cfc1efcf0049c8f2b85a6
Brian Bigdelle <bb586@cornell.edu>
2020-12-27 10:35:26 -0500

    Updated web client

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App(){
-return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+
+function App() {
+  return (
+    <div className="_container">
+      <section className="upper d-flex justify-content-md-end justify-content-center">
+        <div className="balance_box">
+          <span className="balance_label">Current Balance</span>
+          <span className="balance">200$</span>
+        </div>
+      </section>
+      <section className="lower">
+        <div className="lower_section">
+          <a href="/" className="buy_btn">
+            Buy Lotto
+          </a>
+          <div class="form-group">
+            <input
+              type="street"
+              class="form-control"
+              id="inputStreet"
+              placeholder="Street"
+            />
+
+            <input
+              type="city"
+              class="form-control"
+              id="inputCity"
+              placeholder="City"
+            />
+
+            <input
+              type="state"
+              class="form-control"
+              id="inputState"
+              placeholder="State"
+            />
+
+            <input
+              type="zip"
+              class="form-control"
+              id="inputZip"
+              placeholder="Zip"
+            />
+          </div>
+        </div>
+      </section>
     </div>
   );
-  
 }
-  export default App;
+
+export default App;
+
+
+
+
+
--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Board from './Board';
-//import * as serviceWorker from './serviceWorker';
+import { Web3Provider } from 'react-web3';
 
 // import drizzle functions and contract artifact
 import { Drizzle, generateStore } from "@drizzle/store";
@@ -25,5 +24,9 @@ const options = {
 // setup the drizzle store and drizzle
 const drizzle = new Drizzle(options);
 
-//ReactDOM.render(<App drizzle={drizzle} />, document.getElementById('root'));
-ReactDOM.render(<Board drizzle={drizzle} />, document.getElementById('root'));
+ReactDOM.render(<App drizzle={drizzle} />, document.getElementById('root'));
+
+
+
+
+
